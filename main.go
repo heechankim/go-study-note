fa5e4cf603c7ba9ce25d2debc8a931e01e3e159a
heechankim <heechan.dev@gmail.com>
2022-02-24 07:45:09 +0900

    swicth-fallthrough

--- main.go
+++ main.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	var a int = 3
+	var a int = 1
 
 	switch a {
 	case 1:
 		fmt.Println("a is 1")
+		fallthrough
 	case 2:
 		fmt.Println("a is 2")
+		fallthrough
 	case 3:
 		fmt.Println("a is 3")
 	}
