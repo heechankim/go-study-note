bca248d623d67e3202e25594ad7ce7ff68bf7876
heechankim <heechan.dev@gmail.com>
2022-02-26 08:08:38 +0900

    simple function

--- main.go
+++ main.go
@@ -2,19 +2,11 @@ package main
 
 import "fmt"
 
+func area(w, h int) int {
+	return w * h
+}
+
 func main() {
-
-	x := 7
-
-	table := [][]int{{1, 5, 9}, {2, 6, 5, 13}, {5, 3, 7, 4}}
-
-	LOOP:
-	for row := 0; row < len(table); row++ {
-		for col := 0; col < len(table[row]); col++ {
-			if table[row][col] == x {
-				fmt.Printf("found %d(row:%d, col:%d)\n", x, row, col)
-				break LOOP
-			}
-		}
-	}
+	v := area(3, 4)
+	fmt.Println(v)
 }
