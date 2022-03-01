e52b60409dcee86507985c59ee16828c45b7b774
heechankim <heechan.dev@gmail.com>
2022-03-02 08:27:02 +0900

    simple array

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
-	"main/lib"
 )
 
 func main() {
-	fmt.Println(lib.IsDigit('1'))
-	fmt.Println(lib.IsDigit('a'))
+	var a [5]int
+	b := [3]int{1, 2, 3}
+	c := [3]int{1, 2}
+	d := [...]int{4, 5, 6, 7, 8}
+	e := [3][3]int{
+		{1, 2, 3},
+		{3, 4, 5},
+	}
+	fmt.Printf("%-10T %d %v\n", a, len(a), a)
+	fmt.Printf("%-10T %d %v\n", b, len(b), b)
+	fmt.Printf("%-10T %d %v\n", c, len(c), c)
+	fmt.Printf("%-10T %d %v\n", d, len(d), d)
+	fmt.Printf("%-10T %d %v\n", e, len(e), e)
 }
