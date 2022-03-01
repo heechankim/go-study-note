f1c37e71eb30e77c00f746d070c604bc674ee42e
heechankim <heechan.dev@gmail.com>
2022-03-01 17:37:54 +0900

    package init func

--- lib/lib.go
+++ lib/lib.go
@@ -1,5 +1,14 @@
 package lib
 
+import "fmt"
+
+var message string
+
+func init() {
+	message = "this is lib package"
+}
+
 func IsDigit(c int32) bool {
+	fmt.Println(message)
 	return '0' <= c && c <= '9'
 }
