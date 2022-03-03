2ea6d11f6a8ababcbf21cf1a0b5e226aca7e347b
heechankim <heechan.dev@gmail.com>
2022-03-03 09:16:33 +0900

    pointer

--- main.go
+++ main.go
@@ -3,11 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	numbers := []int{3, 4, 5, 7, 8, 4, 6, 8, 32, 4}
-	sum := 0
-	for _, value := range numbers {
-		sum += value
-	}
 
-	fmt.Println("sum: ", sum)
+	var p *int
+	i := 1
+	p = &i
+	fmt.Println(i)
+	fmt.Println(&i)
+	fmt.Println(p)
+	fmt.Println(*p)
 }
