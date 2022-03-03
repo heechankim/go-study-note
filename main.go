3e2f9cd5c5311582e03d7c39001f12757bef0101
heechankim <heechan.dev@gmail.com>
2022-03-03 09:10:48 +0900

    array with for

--- main.go
+++ main.go
@@ -5,21 +5,8 @@ import (
 )
 
 func main() {
-	var a []int
-	b := []int{}
-	c := []int{1, 2, 3}
-	d := [][]int{
-		{1, 2},
-		{3, 4, 5},
+	numbers := []int{3, 4, 5, 7, 8, 4, 6, 8, 32, 4}
+	for index, value := range numbers {
+		fmt.Println(index, value)
 	}
-	e := make([]int, 0)
-	f := make([]int, 3, 5)
-
-
-	fmt.Printf("%-10T %d %d %v\n", a, len(a), cap(a), a)
-	fmt.Printf("%-10T %d %d %v\n", b, len(b), cap(b), b)
-	fmt.Printf("%-10T %d %d %v\n", c, len(c), cap(c), c)
-	fmt.Printf("%-10T %d %d %v\n", d, len(d), cap(d), d)
-	fmt.Printf("%-10T %d %d %v\n", e, len(e), cap(e), e)
-	fmt.Printf("%-10T %d %d %v\n", f, len(f), cap(f), f)
 }
