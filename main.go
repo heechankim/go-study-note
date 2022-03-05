78b77dce53dafe7a1b6730925ea3c965a63f6bdb
heechankim <heechan.dev@gmail.com>
2022-03-05 09:17:32 +0900

    oop in go

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import "fmt"
 
-func main() {
-
-	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	multiply(numbers, 5)
-	fmt.Println(numbers)
+type Item struct {
+	name string
+	price float64
+	quan int
 }
 
-func multiply(numbers []int, factor int) {
-	for i := range numbers {
-		numbers[i] *= factor
-	}
+func (t Item) Cost() float64 {
+	return t.price * float64(t.quan)
+}
+
+func main() {
+
+	shirt := Item{name: "Shirt", price: 25000, quan: 3}
+
+	fmt.Println(shirt.Cost())
+
 }
