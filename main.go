07697cabb232ed8786023bf4e890dd70bb0029ed
heechankim <heechan.dev@gmail.com>
2022-03-07 09:07:06 +0900

    embedded field

--- main.go
+++ main.go
@@ -2,25 +2,26 @@ package main
 
 import "fmt"
 
-type rect struct {
-	w, h float64
+type Option struct {
+	name string
+	value string
+}
+
+type Item struct {
+	name string
+	price float64
+	quantity int
+	Option
 }
 
 func main() {
-	r1 := rect{1, 2}
-	r2 := new(rect)
-	r2.w, r2.h = 3, 4
-	r3 := &rect{}
-	r4 := &rect{5, 6}
+	shoes := Item{"shoes", 30000, 2,
+		Option{"color", "red"}}
 
-	fmt.Printf("%p \n\n", &r1)
+	fmt.Println(shoes)
 
-	fmt.Printf("%p \n", &r2)
-	fmt.Printf("%p \n\n", r2)
+	fmt.Println(shoes.name)
 
-	fmt.Printf("%p \n", &r3)
-	fmt.Printf("%p \n\n", r3)
-
-	fmt.Printf("%p \n", &r4)
-	fmt.Printf("%p \n\n", r4)
+	fmt.Println(shoes.Option.name)
+	fmt.Println(shoes.value)
 }
