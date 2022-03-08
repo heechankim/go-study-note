31aef56fac3347380eb169c426719df7aede4dd5
heechankim <heechan.dev@gmail.com>
2022-03-09 08:29:58 +0900

    interface3

--- main.go
+++ main.go
@@ -29,12 +29,43 @@ func (t DiscountItem) Cost() float64 {
 	return t.Item.Cost() * (1.0 - t.discountRate/100)
 }
 
+type Rental struct {
+	name string
+	feePerDay float64
+	periodLength int
+	RentalPeriod
+}
+type RentalPeriod int
+
+const (
+	Days RentalPeriod = iota
+	Weeks
+	Months
+)
+
+func (p RentalPeriod) ToDays() int {
+	switch p {
+	case Weeks:
+		return 7
+	case Months:
+		return 30
+	default:
+		return 1
+	}
+}
+
+func (r Rental) Cost() float64 {
+	return r.feePerDay * float64(r.ToDays() * r.periodLength)
+}
+
 func main() {
 	shoes := Item{"shoes", 30000, 2}
 	eventShoes := DiscountItem{
 		Item{"eventShoes", 50000, 3},
 		10.00,
 	}
+	video := Rental{"movie", 1000, 3, Weeks}
 	displayCost(shoes)
 	displayCost(eventShoes)
+	displayCost(video)
 }
