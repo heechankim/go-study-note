555a8faa3dd093c4bd78346ab1bd791e60cbf4b2
heechankim <heechan.dev@gmail.com>
2022-03-11 08:13:33 +0900

    simple goroutine

--- main.go
+++ main.go
@@ -1,81 +1,28 @@
 package main
 
-import "fmt"
-
-type Coster interface {
-	Cost() float64
-}
-
-func displayCost(c Coster) {
-	fmt.Println("cost: ", c.Cost())
-}
-
-type Item struct {
-	name string
-	price float64
-	quantity int
-}
-
-func (t Item) Cost() float64 {
-	return t.price * float64(t.quantity)
-}
-
-type DiscountItem struct {
-	Item
-	discountRate float64
-}
-
-func (t DiscountItem) Cost() float64 {
-	return t.Item.Cost() * (1.0 - t.discountRate/100)
-}
-
-type Rental struct {
-	name string
-	feePerDay float64
-	periodLength int
-	RentalPeriod
-}
-type RentalPeriod int
-
-const (
-	Days RentalPeriod = iota
-	Weeks
-	Months
+import (
+	"fmt"
+	"time"
 )
 
-func (p RentalPeriod) ToDays() int {
-	switch p {
-	case Weeks:
-		return 7
-	case Months:
-		return 30
-	default:
-		return 1
-	}
-}
-
-func (r Rental) Cost() float64 {
-	return r.feePerDay * float64(r.ToDays() * r.periodLength)
-}
-
-type Items []Coster
-
-func (ts Items) Cost() (c float64) {
-	for _, t := range ts {
-		c += t.Cost()
-	}
-	return
-}
-
-
 func main() {
-	shoes := Item{"shoes", 25000, 3}
-	eventShoes := DiscountItem{
-		Item{"eventShoes", 50000, 3},
-		10.00,
-	}
-	video := Rental{"movie", 1000, 3, Days}
+	fmt.Println("Start main function", time.Now())
 
-	items := Items{shoes, eventShoes, video}
-	displayCost(items)
+	go long()
+	go short()
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("End main function", time.Now())
+}
+
+func long() {
+	fmt.Println("-- Start long function", time.Now())
+	time.Sleep(3 * time.Second)
+	fmt.Println("-- End long function", time.Now())
+}
+
+func short() {
+	fmt.Println("-- Start short function", time.Now())
+	time.Sleep(1 * time.Second)
+	fmt.Println("-- End short function", time.Now())
 }
