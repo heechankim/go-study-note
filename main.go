9aede52039da956a26f9e2367c6e618da384f1e6
heechankim <heechan.dev@gmail.com>
2022-03-12 17:17:21 +0900

    select

--- main.go
+++ main.go
@@ -2,15 +2,27 @@ package main
 
 import "fmt"
 
-func main() {
-	c := make(chan int, 2)
-	c <- 1
-	c <- 2
-	go func() {
-		c <- 3
-	}()
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c<-x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
 
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+func main() {
+	c := make(chan int)
+	quit := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println(<-c)
+		}
+		quit <- 0
+	}()
+	fibonacci(c, quit)
 }
