1995e8f2e0adb2bad02279925b6aadc03b8bfb23
heechankim <heechan.dev@gmail.com>
2022-03-13 08:57:00 +0900

    goroutine - timeout pattern

--- main.go
+++ main.go
@@ -6,19 +6,30 @@ import (
 )
 
 func main() {
-	tick := time.Tick(100 * time.Millisecond)
-	boom := time.After(500 * time.Millisecond)
+	quit := make(chan struct{})
+	done := process(quit)
+	timeout := time.After(1 * time.Second)
 
-	for {
-		select {
-		case <- tick:
-			fmt.Println("tick.")
-		case <-boom:
-			fmt.Println("BOOM!")
-			return
-		default:
-			fmt.Println(" .")
-			time.Sleep(50 * time.Millisecond)
-		}
+	select {
+	case d := <-done:
+		fmt.Println(d)
+	case <- timeout:
+		fmt.Println("Time Out!")
+		quit <- struct{}{}
 	}
+}
+
+func process(quit <- chan struct{}) chan string {
+	done := make(chan string)
+	go func() {
+		go func() {
+			time.Sleep(10 * time.Second)
+
+			done <- "Complete!"
+		}()
+
+		<- quit
+		return
+	}()
+	return done
 }
