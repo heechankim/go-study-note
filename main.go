c9d932a19ab44b2c723939f7ff2892a16e9bbb04
heechankim <heechan.dev@gmail.com>
2022-03-12 17:13:21 +0900

    buffered channel

--- main.go
+++ main.go
@@ -1,33 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Start main function", time.Now())
+	c := make(chan int, 2)
+	c <- 1
+	c <- 2
+	go func() {
+		c <- 3
+	}()
 
-	done := make(chan bool)
-	go long(done)
-	go short(done)
-
-	<-done
-	<-done
-
-	fmt.Println("End main function", time.Now())
-}
-
-func long(d chan bool) {
-	fmt.Println("-- Start long function", time.Now())
-	time.Sleep(3 * time.Second)
-	fmt.Println("-- End long function", time.Now())
-	d <- true
-}
-
-func short(done chan bool) {
-	fmt.Println("-- Start short function", time.Now())
-	time.Sleep(1 * time.Second)
-	fmt.Println("-- End short function", time.Now())
-	done <- true
+	fmt.Println(<-c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 }
