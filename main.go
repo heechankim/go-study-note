e34c580652daca70e3d8a555381f3cf4fe82a1ca
heechankim <heechan.dev@gmail.com>
2022-03-19 08:20:04 +0900

    error package

--- main.go
+++ main.go
@@ -2,81 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+	"errors"
 )
-
-const (
-	JOB_COUNT = 10
-	BUF_SIZE = 5
-)
-
-type Job struct {
-	name, log string
-}
-type JobHandler func(Job) Job
-func (j Job) String() string {
-	return "job name: " + j.name + "\n[log]\n" + j.log
-}
 
 func main() {
-	done := doLast(doThird(doSecond(doFirst(prepare()))))
-	for d := range done {
-		fmt.Println(d)
-	}
-}
+	errNotFound := errors.New("Not found error")
 
-func prepare() <-chan Job {
-	out := make(chan Job, BUF_SIZE)
-	go func() {
-		for i := 0; i < JOB_COUNT; i++ {
-			out <- Job{name: strconv.Itoa(i)}
-		}
-		close(out)
-	}()
-	return out
-}
-
-func doFirst(in <-chan Job) <-chan Job {
-	out := make(chan Job, cap(in))
-	go func() {
-		for job := range in {
-			job.log += "first stage\n"
-			out <- job
-		}
-		close(out)
-	}()
-	return out
-}
-func doSecond(in <-chan Job) <-chan Job {
-	out := make(chan Job, cap(in))
-	go func() {
-		for job := range in {
-			job.log += "second stage\n"
-			out <- job
-		}
-		close(out)
-	}()
-	return out
-}
-func doThird(in <-chan Job) <-chan Job {
-	out := make(chan Job, cap(in))
-	go func() {
-		for job := range in {
-			job.log += "third stage\n"
-			out <- job
-		}
-		close(out)
-	}()
-	return out
-}
-func doLast(in <-chan Job) <-chan Job {
-	out := make(chan Job, cap(in))
-	go func() {
-		for job := range in {
-			job.log += "last stage\n"
-			out <- job
-		}
-		close(out)
-	}()
-	return out
+	fmt.Println("error: ", errNotFound)
+	fmt.Println("error: ", errNotFound.Error())
 }
