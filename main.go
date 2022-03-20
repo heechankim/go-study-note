741fe8f7b794e1e77e4cea719b31872dcf59520a
heechankim <heechan.dev@gmail.com>
2022-03-20 09:23:27 +0900

    custom error handler

--- main.go
+++ main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"math"
+	"time"
 )
+
+type SqrtError struct {
+	time time.Time
+	value float64
+	message string
+}
+
+func (e SqrtError) Error() string {
+	return fmt.Sprintf("[%v]ERROR - %s(value: %g)", e.time, e.message, e.value)
+}
 
 func Sqrt(f float64) (float64, error) {
 	if f < 0 {
-		return 0, fmt.Errorf("Cannot use negative integer (%g)", f)
+		return 0, SqrtError{time.Now(), f, "cannot use n value"}
 	}
 	return math.Sqrt(f), nil
 }
 
 func main() {
 	if f, err := Sqrt(-1); err != nil {
-		fmt.Printf("Error: %s\n", err)
+		log.Fatal(err)
 	} else {
 		fmt.Println(f)
 	}
