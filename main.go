83b905bb9b748858731bf093596032260cfdf1c2
heechankim <heechan.dev@gmail.com>
2022-03-21 08:14:09 +0900

    panic and recover

--- main.go
+++ main.go
@@ -1,33 +1,15 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"math"
-	"time"
-)
-
-type SqrtError struct {
-	time time.Time
-	value float64
-	message string
-}
-
-func (e SqrtError) Error() string {
-	return fmt.Sprintf("[%v]ERROR - %s(value: %g)", e.time, e.message, e.value)
-}
-
-func Sqrt(f float64) (float64, error) {
-	if f < 0 {
-		return 0, SqrtError{time.Now(), f, "cannot use n value"}
-	}
-	return math.Sqrt(f), nil
-}
+import "fmt"
 
 func main() {
-	if f, err := Sqrt(-1); err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println(f)
-	}
+	fmt.Println(divide(1, 0))
+}
+func divide(a, b int) int {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+	return a / b
 }
